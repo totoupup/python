7f1aef0a3e26f2389ba39a44559d61d158d646d9
e <wangyuanhpu@163.com>
2019-06-14 21:35:35 +0800

    use box sizer

--- code/pratice/02wxpython/ModifyConfigPath.py
+++ code/pratice/02wxpython/ModifyConfigPath.py
@@ -9,13 +9,22 @@ import wx
 class ModifyConfigPath(wx.Frame):
     def __init__(self):
         # frame = wx.Frame(None, title='Modify Config Path', pos=(400, 400), size=(400, 400))
-        frame = wx.Frame(None, title='Modify Config Path', pos=(400, 300), size=(400, 400))
-        path_text = wx.TextCtrl(frame, pos=(5, 5), size=(260, 24))
-        open_button = wx.Button(frame, label="打开", pos=(270, 5), size=(50, 24))
-        save_button = wx.Button(frame, label="保存", pos=(330, 5), size=(50, 24))
+        frame = wx.Frame(None, title='Modify Config Path', pos=(400, 300), size=(600, 500))
+        panel = wx.Panel(frame)
+        path_text = wx.TextCtrl(panel)
+        open_button = wx.Button(panel, label="打开")
+        save_button = wx.Button(panel, label="保存")
         # wx.TE_MULTILINE多行显示
-        content_text = wx.TextCtrl(frame, pos=(5, 35), size=(375, 320),
-                               style=wx.TE_MULTILINE)
+        content_text = wx.TextCtrl(panel, style=wx.TE_MULTILINE)
+        box = wx.BoxSizer(wx.HORIZONTAL)  # 不带参数默认实例化一个水平的尺寸器
+        box.Add(path_text, proportion=5, flag=wx.EXPAND|wx.ALL, border=3)
+        box.Add(open_button, proportion=2, flag=wx.EXPAND|wx.ALL, border=3)
+        box.Add(save_button, proportion=2, flag=wx.EXPAND|wx.ALL, border=3)
+
+        v_box = wx.BoxSizer(wx.VERTICAL)
+        v_box.Add(box, proportion=1, flag=wx.EXPAND|wx.ALL, border=3)
+        v_box.Add(content_text, proportion=3,flag=wx.EXPAND|wx.ALL, border=3)
+        panel.SetSizer(v_box)
 
         def openfile(event):  # 参数必须为event，其他参数如何添加？
             filesFilter = "Txt (*.txt)|*.txt|All files (*.*)|*.*"  # 文件格式过滤器
@@ -26,7 +35,7 @@ class ModifyConfigPath(wx.Frame):
             path = fileDialog.GetPath()
             path_text.SetValue(path)
             path = path_text.GetValue()
-            with open(path, 'r', encoding='default') as f:
+            with open(path, 'r', encoding='utf-8') as f:
                 content_text.SetValue(f.read())
         open_button.Bind(wx.EVT_BUTTON, openfile)
         frame.Show()
